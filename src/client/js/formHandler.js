975ff03cfc9fd114d67922b1ed73de4bbe92406b
HoangTran <40290572+hoangtran-97@users.noreply.github.com>
2020-01-10 15:12:46 +0200

    feat: split post function

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,3 +1,4 @@
+import postData from "./postData"
 const handleSubmit = async (event) => {
     event.preventDefault()
     const textInput = document.getElementById('textData').value
@@ -10,21 +11,6 @@ const handleSubmit = async (event) => {
     postData("http://localhost:8081/sentiment", textData)
     const request = await fetch('http://localhost:8081/all');
 }
-const postData = async (url = "", data = {}) => {
-    const response = await fetch(url, {
-        method: "POST",
-        credentials: "same-origin",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
-    });
-    try {
-        const newData = await response.json();
-        return newData
-    } catch (error) {
-        console.log("error", error);
-    }
-}
+
 
 export { handleSubmit }
